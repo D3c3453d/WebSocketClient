3e5c7ff08281bb818259f85a11e5b7f2960421b1
D3c3453d <sergey.et02@gmail.com>
2022-09-14 17:53:45 +0300

    feat: working client

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
+)
+
+func listener(conn *websocket.Conn) {
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			logrus.Error("Error during message reading: ", err)
+		}
+		logrus.Infof("Received from %s: %s", conn.RemoteAddr().String(), message)
+	}
+}
+
+func writer(conn *websocket.Conn) {
+	var input string
+	for {
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			logrus.Error("Error scan: ", err)
+		}
+		err = conn.WriteMessage(1, []byte(input))
+		if err != nil {
+			logrus.Error("Error during message writing: ", err)
+		}
+	}
+}
+
+func main() {
+	socketUrl := "ws://localhost:7077" + "/ws"
+	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		logrus.Fatal("Error connecting to Websocket Server:", err)
+	}
+	go listener(conn)
+	writer(conn)
+}
