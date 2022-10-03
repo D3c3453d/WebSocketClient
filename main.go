220b615da15ba65890867de31692119eabe92f6d
D3c3453d <sergey.et02@gmail.com>
2022-10-03 19:39:07 +0300

    fix: input

--- main.go
+++ main.go
@@ -1,59 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func listener(conn *websocket.Conn) {
 	for {
-		_, username, err := conn.ReadMessage()
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			logrus.Error("Error during message reading: ", err)
 		}
-		logrus.Infof("Received from %s: %s", username, message)
+		logrus.Infof("Received from %s\n", message)
+	}
+}
+
+func ponger(conn *websocket.Conn) {
+	for {
+		if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+			return
+		}
 	}
 }
 
 func writer(conn *websocket.Conn) {
-	var username string
-	var input string
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		_, err := fmt.Scanf("%s %s", &username, &input)
-		if err != nil {
+		scanner.Scan()
+		input := scanner.Text()
+		//logrus.Info(input)
+		if err := scanner.Err(); err != nil {
 			logrus.Error("Error scan: ", err)
+			break
 		}
-		err = conn.WriteMessage(1, []byte(username))
-		err = conn.WriteMessage(1, []byte(input))
+		err := conn.WriteMessage(1, []byte(input))
 		if err != nil {
 			logrus.Error("Error during message writing: ", err)
 		}
+
 	}
 }
 
 func main() {
 	var username string
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Please enter your username: ")
-		_, err := fmt.Scan(&username)
-		if err != nil {
-			logrus.Error("Wrong username: ", err)
+		scanner.Scan()
+		username = scanner.Text()
+		if err := scanner.Err(); err != nil {
+			logrus.Error("Error scan: ", err)
 		} else {
 			break
 		}
 	}
 
-	socketUrl := "ws://localhost:7077" + "/ws"
+	socketUrl := "ws://localhost:7078" + "/ws"
 	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		logrus.Fatal("Error connecting to Websocket Server:", err)
+	}
 	err = conn.WriteMessage(1, []byte(username))
 	if err != nil {
 		logrus.Error("Error during username writing: ", err)
 	}
-	if err != nil {
-		logrus.Fatal("Error connecting to Websocket Server:", err)
-	}
+
+	//go ponger(conn)
 	go listener(conn)
 	writer(conn)
 }
